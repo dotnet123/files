293a2045b6f608c9e7accc1fde80a3b87b3b9e04
zhaoming <275169263@qq.com>
2018-03-12 17:20:11 +0800

    尝试前端调用onpost,失败

---
+++ WebApplication1/Models/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        [LiteDB.BsonField("companyname")]
+        public string CompanyName { get; set; }
+
+        [LiteDB.BsonField("address")]
+        public string Address { get; set; }
+
+        [LiteDB.BsonField("contacts")]
+        public string Contacts { get; set; }
+
+
+        [LiteDB.BsonField("tel")]
+        public string Tel { get; set; }
+
+        [LiteDB.BsonField("contractamount")]
+        public decimal ContractAmount { get; set; }
+
+
+        [LiteDB.BsonField("huikuanstatus")]
+        public int HuiKuanStatus { get; set; }
+
+
+        [LiteDB.BsonField("visitrecordlist")]
+        public List<Tuple<DateTime,string>> VisitRecordList { get; set; }
+
+        [LiteDB.BsonField("huikuanlist")]
+        public List<Tuple<DateTime, string>> HuiKuanList { get; set; }
+
+
+        [LiteDB.BsonField("logtime")]
+        public DateTime LogTime { get; set; }
+
+        [LiteDB.BsonField("seqno")]
+        public int SeqNo { get; set; }
+        [LiteDB.BsonField("status")]
+        public int Status { get; set; }
+    }
+}
---
+++ WebApplication1/Pages/Customer/Add.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages.Customer
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public IActionResult OnPostAdd()
+        {
+            var stream = Request.Body.ToString();
+
+            return RedirectToPage();
+        }
+        
+    }
+}
